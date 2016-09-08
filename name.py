438bfb258fcdb538f7765258e6433cd1a5c13989
dzieber <dan.zieber@gmail.com>
2016-09-08 16:58:06 -0700

    added introspector tool

---
+++ name.py
@@ -0,0 +1,13 @@
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + " " + last_name
+print(full_name.title())
+full_name = full_name.title()
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print full_name.lower()
+print "Hello, " + full_name.title() +"!"
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
