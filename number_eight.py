714129dade92c894d17921058b4eeb18e9f94411
dzieber <dan.zieber@gmail.com>
2016-09-09 22:13:16 -0700

    exercise 2.10

--- number_eight.py
+++ number_eight.py
@@ -1,3 +1,8 @@
+"""
+This prints the number 8 using the basic numerical operators.
+Notice that this is written in 2.7, so the division by default
+produces truncated integers.
+"""
 print(5 + 3)
 print(4 / 2 + 6)
 print(3 / 2 + 7)
