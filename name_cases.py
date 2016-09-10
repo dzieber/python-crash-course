2c538b81c4f1ad6876bed5df3d8e913daabc0e40
dzieber <dan.zieber@gmail.com>
2016-09-09 21:13:09 -0700

    completed exercise 2.3
    exercise 2.4
    
    exercise 2.5
    
    exercise 2.6
    
    exercise 2.7

---
+++ name_cases.py
@@ -0,0 +1,5 @@
+person_name = "Buffy McGee"
+print("original" + " " + person_name)
+print("lowercase" + " " + person_name.lower())
+print("uppercase" + " " + person_name.upper())
+print("title" + " " + person_name.title())
---
+++ stripping_names.py
@@ -0,0 +1,6 @@
+person_name = "\n\tAlabaster Jones the Third     "
+print("unmodified:" + " " + person_name)
+print("lstrip:" + " " + person_name.lstrip())
+print("rstrip:" + " " + person_name.rstrip())
+print("strip:" + " " + person_name.strip())
+print("rstriplstrip:" + " " + person_name.rstrip().lstrip())
