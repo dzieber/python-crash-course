c20d6a0e99c9c756712c41ca05712dd54631cddc
dzieber <dan.zieber@gmail.com>
2016-09-11 21:39:45 -0700

    initial commit of squids.py, working on adding and deleting list elements

---
+++ ch3/squids.py
@@ -0,0 +1,9 @@
+'''
+Working on adding and removing elements from lists
+'''
+squids = ['humbolt', 'fried', 'calamari', 'poke']
+print(squids)
+squids[0] = 'tasty'
+print(squids)
+squids.append('buttered')
+print(squids)
