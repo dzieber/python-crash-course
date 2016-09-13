684d99e28ec6d3198b483930ef4bc0364e4a7c03
dzieber <dan.zieber@gmail.com>
2016-09-12 21:21:29 -0700

    chapter 3, work and exercises 3.4 to 3.7

---
+++ ch3/guest_list.py
@@ -0,0 +1,34 @@
+'''
+the program for exercies 3.4 - 3.7
+'''
+
+guest_list = ['ME', 'myself', 'i']
+message = "I would love to invite to dinner " + guest_list[0].title() + ", " \
+    + guest_list[1].title() + ", and " + guest_list[-1].upper() + "."
+print(message)
+no_show = 'i'
+print("Oh noes, " + no_show.upper() + " can't make it!")
+new_guest = 'yo'
+guest_list.append(new_guest)
+guest_list.remove(no_show)
+message = "I would love to invite to dinner " + guest_list[0].title() + ", " \
+    + guest_list[1].title() + ", and " + guest_list[-1].upper() + "."
+print(message)
+print("I done found me a bigger table!")
+guest_list.insert(0,"god_king")
+guest_list.insert(2,"master")
+guest_list.append("lord")
+message = "I would love to invite to dinner " + guest_list[0].title() + ", " \
+    + guest_list[1].title() + ", " + guest_list[-1].upper() + ", " +\
+    guest_list[-2] + ", " + guest_list[-3] + ", and " + guest_list[-4] + "."
+print(message)
+print("Oh noes!  My table shrunk!")
+print("Sorry " + guest_list.pop() + ", you outta here!")
+print("Sorry " + guest_list.pop() + ", you outta here!")
+print("Sorry " + guest_list.pop() + ", you outta here!")
+print("Sorry " + guest_list.pop() + ", you outta here!")
+print("Hey "+ guest_list[0].title() + " and " + guest_list[-1].title() + \
+    ", c'mon bye!")
+del guest_list[0]
+del guest_list[0]
+print(guest_list)
--- ch3/squids.py
+++ ch3/squids.py
@@ -7,3 +7,17 @@ squids[0] = 'tasty'
 print(squids)
 squids.append('buttered')
 print(squids)
+del squids[3]
+print(squids)
+squids.insert(2,squids.pop())
+print(squids)
+squids.insert(1,'yucky')
+print(squids)
+message = "I don't like " + squids.pop(1) + " squids."
+print(message)
+not_prep = 'tasty'
+squids.remove(not_prep)
+print(squids)
+message = "I think that squid is " + not_prep + " when it is " + squids[0] + \
+        " and " + squids[1] + "."
+print(message)
